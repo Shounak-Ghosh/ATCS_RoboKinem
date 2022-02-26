935d3d5b024284d71d3e76c128a700618fe5ffd6
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-02-26 15:56:08 -0800

    modularized code and included main method

--- simulation/circle.py
+++ simulation/circle.py
@@ -1,21 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Set the plot boundaries
-plt.figure("Circle", figsize=(8, 8))
-plt.xlim(-2, 2)
-plt.ylim(-2, 2)
-
 
 # Draws a circle given 2 points forming the diameter
-def circle(point_a, point_b):
+def generate_circle(point_a, point_b):
     point_list = []
-    xDist = point_b[0] - point_a[0]
-    yDist = point_b[1] - point_a[1]
-    dist = (xDist ** 2 + yDist ** 2) ** .5
+    x_dist = point_b[0] - point_a[0]
+    y_dist = point_b[1] - point_a[1]
+    dist = (x_dist ** 2 + y_dist ** 2) ** .5
     numpoints = 60
     t = np.linspace(0, 2 * np.pi, numpoints)
-    sign = -1 if xDist > 0 else 1
+    sign = -1 if x_dist > 0 else 1
 
     for i in range(numpoints):
         x = (point_a[0] + point_b[0]) / 2 + sign * (dist / 2) * np.cos(t[i])
@@ -28,9 +23,22 @@ def plot_circle(point_list):
     for point in point_list:
         plt.plot(point[0], point[1], 'bo')
         plt.draw()
-        plt.pause(.01)
+        plt.pause(1)
     plt.show()
 
+def main():
+    # Set the plot boundaries
+    plt.figure("Circle", figsize=(8, 8))
+    plt.xlim(-2, 2)
+    plt.ylim(-2, 2)
 
-plot_circle(circle([-1, 0], [1, 0]))
-plt.show()
+    point_list = generate_circle([-1, 0], [1, 0])
+    print(point_list)
+    plot_circle(point_list)
+
+
+if __name__ == "__main__":
+   main()
+
+
+
