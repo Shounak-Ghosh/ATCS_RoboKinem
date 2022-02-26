ee685777080859b705f5096cac5fecf082f61c5d
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-02-26 10:10:00 -0800

    update code to seperate point list generation and plotting

--- simulation/circle.py
+++ simulation/circle.py
@@ -9,20 +9,28 @@ plt.ylim(-2, 2)
 
 # Draws a circle given 2 points forming the diameter
 def circle(point_a, point_b):
+    point_list = []
     xDist = point_b[0] - point_a[0]
     yDist = point_b[1] - point_a[1]
     dist = (xDist ** 2 + yDist ** 2) ** .5
-    numpoints = 360
+    numpoints = 60
     t = np.linspace(0, 2 * np.pi, numpoints)
     sign = -1 if xDist > 0 else 1
 
     for i in range(numpoints):
         x = (point_a[0] + point_b[0]) / 2 + sign * (dist / 2) * np.cos(t[i])
         y = (point_a[1] + point_b[1]) / 2 + sign * (dist / 2) * np.sin(t[i])
-        plt.plot(x, y, 'bo')
+        point_list.append([x, y])
+    return point_list
+
+
+def plot_circle(point_list):
+    for point in point_list:
+        plt.plot(point[0], point[1], 'bo')
         plt.draw()
-        plt.pause(1)
+        plt.pause(.01)
+    plt.show()
 
 
-circle([-1, 0], [1, 0])
+plot_circle(circle([-1, 0], [1, 0]))
 plt.show()
