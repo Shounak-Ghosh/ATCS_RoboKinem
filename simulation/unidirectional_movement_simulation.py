254b1ec0b3849c76051ee6c0e3ce62bf6e41cd1a
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-02-26 22:37:45 -0800

    moved file to simulation subdirectory

--- simulation/unidirectional_movement_simulation.py
+++ simulation/unidirectional_movement_simulation.py
@@ -1,48 +1,30 @@
-import os
-import sys
 import matplotlib.pyplot as plt
 import numpy as np
-import time
-
-script_dir = os.path.dirname(__file__)
-simulation_dir = os.path.join(script_dir, '..', 'simulation')
-sys.path.append(simulation_dir)
 import circle as c
 import loop as l
+import time
 
 
-def method1():
-    n = 360
-    for i in range(n):
-        x = np.cos(i * np.pi / 180)  # converting degrees to radians
-        y = np.sin(i * np.pi / 180)
-        # plt.plot(x, y,'bo')
-        plt.plot(i, x, 'ro')
-        plt.plot(i, y, 'go')
-        time.sleep(.01)
-        plt.draw()
-    plt.show()
-
-
-def method2():
-    point_list = c.generate_circle([-1, 0], [1, 0])
-    # vector_list = []
+def plot_vectors(point_list):
     for i in range(len(point_list) - 1):
         x_dist = point_list[i + 1][0] - point_list[i][0]
         y_dist = point_list[i + 1][1] - point_list[i][1]
         # vector_list.append([x_dist,y_dist])
         # plt.plot(x_dist,y_dist,'bo')
-        plt.plot(i, x_dist, 'ro') # should be sin
-        plt.plot(i, y_dist, 'go') # should be -cos
+        plt.plot(i, x_dist, 'ro')  # should be sin
+        plt.plot(i, y_dist, 'go')  # should be -cos
         plt.draw()
-    plt.show()
+
 
 
 def main():
     plt.figure("Circle Vector Simulation", figsize=(8, 8))
-    # plt.xlim(-2, 2)
-    # plt.ylim(-2, 2)
-    method2()
+    plot_vectors(c.generate_circle([-1, 0], [1, 0]))
+
+    plt.figure("Loop Vector Simulation", figsize=(8, 8))
+    plot_vectors(l.generate_loop([1, 1], [-1, 1], [-1, -1], [1, -1]))
+    plt.show()
+
 
 
 if __name__ == '__main__':
