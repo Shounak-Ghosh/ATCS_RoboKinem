00668ca7e626212c695d7cc9d2e3b4d33cd6d413
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-03-08 11:51:35 -0800

    finished unidirectional movement

--- simulation/unidirectional_movement_simulation.py
+++ simulation/unidirectional_movement_simulation.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math
 import numpy as np
 import circle as c
 import shapes as s
@@ -11,21 +12,34 @@ def plot_vectors(point_list):
         y_dist = point_list[i + 1][1] - point_list[i][1]
         # vector_list.append([x_dist,y_dist])
         # plt.plot(x_dist,y_dist,'bo')
-        plt.plot(i, x_dist, 'ro')  # should be sin
-        plt.plot(i, y_dist, 'go')  # should be -cos
+        vector_length = math.sqrt(x_dist ** 2 + y_dist ** 2)
+        if vector_length != 0:
+            scale = 50 / vector_length
+        else:
+            scale = 1
+        x_dist *= scale
+        y_dist *= scale
+        print(x_dist, y_dist, y_dist - x_dist, y_dist + x_dist)
+        plt.plot(i, x_dist, 'ro')  # should be sin (for circle)
+        plt.plot(i, y_dist, 'g^')  # should be -cos (for circle)
         plt.draw()
 
 
 def main():
-    plt.figure("Circle Vector Simulation", figsize=(8, 8))
-    plt.ylabel("Vector values")
-    plt.xlabel("Time")
-    plot_vectors(c.generate_circle([-1, 0], [1, 0]))
+    # c_scale = 2000  # simply using 1 leads to very small vector values
+    # plt.figure("Circle Vector Simulation", figsize=(8, 8))
+    # plt.ylabel("Vector values")
+    # plt.xlabel("Time")
+    # plot_vectors(c.generate_circle([-c_scale, 0], [c_scale, 0]))
 
+    l_scale = 1
     plt.figure("Loop Vector Simulation", figsize=(8, 8))
     plt.ylabel("Vector values")
     plt.xlabel("Time")
     plot_vectors(s.generate_loop([1, 1], [-1, 1], [-1, -1], [1, -1]))
+    # Diagonal loop
+    # plot_vectors(s.generate_loop([l_scale, 0], [0, l_scale], [-l_scale, 0],
+    #                              [0, -l_scale]))
     plt.show()
 
 
