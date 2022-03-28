a9ab461b950582d8752bcfaa81d677527f1a3c8c
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-03-27 18:49:26 -0700

    rename files (was incompatible with import)

--- matrices/ex_315.py
+++ matrices/ex_315.py
@@ -1,8 +1,15 @@
 import numpy as np
 import math
+import ex_32
+
+
 
 
 def main():
+    print()
+    """
+    Main function
+    """
     t_ab = np.array(
         [[1 / 2 ** .5, -1 / 2 ** .5, 0, -1], [1 / 2 ** .5, 1 / 2 ** .5, 0, 0],
          [0, 0, 1, 1], [0, 0, 0, 1]])
@@ -12,8 +19,7 @@ def main():
     t_ba = np.linalg.inv(t_ab)
     t_ca = t_cb * t_ba
     print("\npart (c)")
-    print(t_ca)
-
+    print(t_ca) 
 
 
 if __name__ == '__main__':
