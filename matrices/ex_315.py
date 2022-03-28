7ccdb7f659fa6817e8c4dfce8fb3167304261bd0
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-03-27 19:02:32 -0700

    code for part(c)

--- matrices/ex_315.py
+++ matrices/ex_315.py
@@ -1,12 +1,21 @@
 import numpy as np
 import math
-import ex_32
+import ex_32 as rot
 
 
+def transformation_mat(rot_mat, p):
+    """
+    Returns the transformation matrix for the rotation matrix and point p
+    @param rot_mat: rotation matrix
+    @param p: point in homogeneous coordinates
+    @return: transformation matrix
+    """
+    t = np.array([[1, 0, 0, p[0]], [0, 1, 0, p[1]],
+                 [0, 0, 1, p[2]], [0, 0, 0, 1]])
+    return np.matmul(rot_mat, t)
 
 
 def main():
-    print()
     """
     Main function
     """
@@ -17,9 +26,9 @@ def main():
     t_cb = np.array([[1 / 2 ** .5, 0, 1 / 2 ** .5, 0], [0, 1, 0, 1],
                      [-1 / 2 ** .5, 0, 1 / 2 ** .5, 0], [0, 0, 0, 1]])
     t_ba = np.linalg.inv(t_ab)
-    t_ca = t_cb * t_ba
+    t_ca = np.matmul(t_cb, t_ba)
     print("\npart (c)")
-    print(t_ca) 
+    print(t_ca)
 
 
 if __name__ == '__main__':
