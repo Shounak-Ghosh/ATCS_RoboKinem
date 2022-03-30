ce4a4e62a701ded48364d18728f7ef1cfcc95cb5
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-03-30 10:00:47 -0700

    added documentation

--- matrices/ex_315.py
+++ matrices/ex_315.py
@@ -5,13 +5,13 @@ import ex_32 as rot
 
 def transformation_mat(rot_mat, p):
     """
-    Returns the transformation matrix for the rotation matrix and point p
-    @param rot_mat: rotation matrix
-    @param p: point in homogeneous coordinates
-    @return: transformation matrix
-    """
+     Returns the transformation matrix for the rotation matrix and point p
+     @param rot_mat: rotation matrix
+     @param p: point in homogeneous coordinates
+     @return: transformation matrix
+     """
     t = np.array([[1, 0, 0, p[0]], [0, 1, 0, p[1]],
-                 [0, 0, 1, p[2]], [0, 0, 0, 1]])
+                  [0, 0, 1, p[2]], [0, 0, 0, 1]])
     return np.matmul(rot_mat, t)
 
 
